d86601cbf8e3ccf05d66310a57dfe9bd4369da65
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2021-11-28 00:15:44 +0530

    Test commit

--- bot.js
+++ bot.js
@@ -2,13 +2,19 @@ const { Telegraf } = require("telegraf");
 require("dotenv").config();
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
-console.log(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply("Welcome!"));
-bot.help((ctx) => ctx.reply("Schedule an event..!"));
-bot.command("events", (ctx) => ctx.reply("Instructions Here....!"));
+const UrlBase =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://events-buddy-bot.herokuapp.com";
+
+async function AboutBot() {
+  bot.start((ctx) => ctx.reply("Welcome!"));
+  bot.help((ctx) => ctx.reply("Schedule an event..!"));
+  bot.command("events", (ctx) => ctx.reply("Instructions Here....!"));
+}
 
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
-module.exports = { bot };
+module.exports = { bot, UrlBase, AboutBot };
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
-const { bot } = require("./bot");
+const { bot, UrlBase } = require("./bot");
 
 app.get("/", (request, response) => {
   response.send("Bot is running");
 });
 
 bot.launch();
+
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
