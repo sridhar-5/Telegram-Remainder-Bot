132d7a4800c3c9644b31bc174660d3308faa8168
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2021-11-27 23:15:32 +0530

    initial commit

--- bot.js
+++ bot.js
@@ -4,6 +4,11 @@ require("dotenv").config();
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 console.log(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.command("help", (ctx) => ctx.reply("Instructions Here....!"));
+bot.start((ctx) => ctx.reply("Welcome!"));
+bot.help((ctx) => ctx.reply("Schedule an event..!"));
+bot.command("events", (ctx) => ctx.reply("Instructions Here....!"));
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
 module.exports = { bot };
