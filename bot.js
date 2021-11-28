ff556f1a49a71d2f384aeec741cd790d8bc24061
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2021-11-28 14:31:46 +0530

    calender commit

--- bot.js
+++ bot.js
@@ -31,12 +31,10 @@ async function Bot() {
         ctx.reply(
           `Okayyy..${ctx.chat.username}! I will sure make a note about ${task}`
         );
-
-        // listen for the selected date event
-        calendar.setDateListener((context, date) => context.reply(date));
-
         // retreive the calendar HTML
         ctx.reply("When is this task due ?", calendar.getCalendar());
+        // listen for the selected date event
+        calendar.setDateListener((context, date) => context.reply(date));
       }
     });
   });
