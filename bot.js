e9a6b408aeec555173cd9d964b813874c73683c9
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2021-12-01 00:57:38 +0530

    Database integration left

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const { Telegraf } = require("telegraf");
+const { Keyboard, Key } = require("telegram-keyboard");
 const axios = require("axios");
 const Calendar = require("telegraf-calendar-telegram");
 require("dotenv").config();
@@ -47,14 +48,21 @@ async function Bot() {
         );
 
         ctx.replyWithMarkdown(
-          "Almost there..! To pick a date reply with <br /> `/calendar`"
+          "Yay..!Almost there, To pick a date reply with `/calendar`"
         );
       }
     });
   });
 
+  var ScheduleDate = "";
+
   // listen for the selected date event
-  calendar.setDateListener((context, date) => context.reply(date));
+  calendar.setDateListener((context, date) => {
+    ScheduleDate = date;
+    context.replyWithMarkdown(`Selected date is ${date}`);
+    context.reply("Donezo..! To set the time reply with `/time`");
+  });
+
   // retreive the calendar HTML
   bot.command("calendar", (context) => {
     const today = new Date();
@@ -67,6 +75,174 @@ async function Bot() {
     context.reply(
       "There you go..! Pick a Date",
       calendar.setMinDate(minDate).setMaxDate(maxDate).getCalendar()
+    );
+  });
+
+  // setting up time starts here
+  Buttons = ["↑", "↓"];
+  AMPM = ["AM", "PM"];
+
+  var currentHours = "12";
+  var currentMinutes = "00";
+  var currentMeridium = "AM";
+
+  var keyboard = Keyboard.make(
+    [
+      Key.callback(Buttons[0], "incHours"),
+      Key.callback(Buttons[0], "incMinutes"),
+      Key.callback(Buttons[0], "AMPM"),
+      Key.callback(currentHours, "Noaction"),
+      Key.callback(currentMinutes, "Noaction"),
+      Key.callback(currentMeridium, "Noaction"),
+      Key.callback(Buttons[1], "DecHours"),
+      Key.callback(Buttons[1], "DecMinut"),
+      Key.callback(Buttons[1], "AMPM"),
+      Key.callback("Okay", "Proceed"),
+    ],
+    {
+      columns: 3,
+    }
+  ).inline();
+
+  bot.command("picktime", (ctx) => {
+    ctx.reply("time", keyboard);
+  });
+
+  bot.action("incHours", (ctx) => {
+    const keyboard_str = keyboard.reply_markup.inline_keyboard;
+    var currentHrs = parseInt(keyboard_str[1][0].text) + 1;
+    if (currentHrs > 12) currentHrs = 1;
+    currentHours = String(currentHrs);
+    keyboard = Keyboard.make(
+      [
+        Key.callback(Buttons[0], "incHours"),
+        Key.callback(Buttons[0], "incMinutes"),
+        Key.callback(Buttons[0], "AMPM"),
+        Key.callback(currentHours, "Noaction"),
+        Key.callback(currentMinutes, "Noaction"),
+        Key.callback(currentMeridium, "Noaction"),
+        Key.callback(Buttons[1], "DecHours"),
+        Key.callback(Buttons[1], "DecMinutes"),
+        Key.callback(Buttons[1], "AMPM"),
+        Key.callback("Okay", "Proceed"),
+      ],
+      {
+        columns: 3,
+      }
+    ).inline();
+    ctx.editMessageReplyMarkup(keyboard.reply_markup);
+  });
+
+  bot.action("DecHours", (ctx) => {
+    const keyboard_str = keyboard.reply_markup.inline_keyboard;
+    var currentHrs = parseInt(keyboard_str[1][0].text) - 1;
+    if (currentHrs < 1) currentHrs = 12;
+    currentHours = String(currentHrs);
+    keyboard = Keyboard.make(
+      [
+        Key.callback(Buttons[0], "incHours"),
+        Key.callback(Buttons[0], "incMinutes"),
+        Key.callback(Buttons[0], "AMPM"),
+        Key.callback(currentHours, "Noaction"),
+        Key.callback(currentMinutes, "Noaction"),
+        Key.callback(currentMeridium, "Noaction"),
+        Key.callback(Buttons[1], "DecHours"),
+        Key.callback(Buttons[1], "DecMinut"),
+        Key.callback(Buttons[1], "AMPM"),
+        Key.callback("Okay", "Proceed"),
+      ],
+      {
+        columns: 3,
+      }
+    ).inline();
+    ctx.editMessageReplyMarkup(keyboard.reply_markup);
+  });
+
+  bot.action("incMinutes", (ctx) => {
+    const keyboard_str = keyboard.reply_markup.inline_keyboard;
+    var currentMins = parseInt(keyboard_str[1][1].text) + 1;
+    if (currentMins > 60) currentMins = 1;
+    currentMinutes = String(currentMins);
+    keyboard = Keyboard.make(
+      [
+        Key.callback(Buttons[0], "incHours"),
+        Key.callback(Buttons[0], "incMinutes"),
+        Key.callback(Buttons[0], "AMPM"),
+        Key.callback(currentHours, "Noaction"),
+        Key.callback(currentMinutes, "Noaction"),
+        Key.callback(currentMeridium, "Noaction"),
+        Key.callback(Buttons[1], "DecHours"),
+        Key.callback(Buttons[1], "DecMinut"),
+        Key.callback(Buttons[1], "AMPM"),
+        Key.callback("Okay", "Proceed"),
+      ],
+      {
+        columns: 3,
+      }
+    ).inline();
+    ctx.editMessageReplyMarkup(keyboard.reply_markup);
+  });
+
+  bot.action("DecMinutes", (ctx) => {
+    const keyboard_str = keyboard.reply_markup.inline_keyboard;
+    var currentMins = parseInt(keyboard_str[1][1].text) - 1;
+    if (currentMins < 0) currentMins = 59;
+    currentMinutes = String(currentMins);
+    keyboard = Keyboard.make(
+      [
+        Key.callback(Buttons[0], "incHours"),
+        Key.callback(Buttons[0], "incMinutes"),
+        Key.callback(Buttons[0], "AMPM"),
+        Key.callback(currentHours, "Noaction"),
+        Key.callback(currentMinutes, "Noaction"),
+        Key.callback(currentMeridium, "Noaction"),
+        Key.callback(Buttons[1], "DecHours"),
+        Key.callback(Buttons[1], "DecMinut"),
+        Key.callback(Buttons[1], "AMPM"),
+        Key.callback("Okay", "Proceed"),
+      ],
+      {
+        columns: 3,
+      }
+    ).inline();
+    ctx.editMessageReplyMarkup(keyboard.reply_markup);
+  });
+
+  bot.action("AMPM", (ctx) => {
+    const keyboard_str = keyboard.reply_markup.inline_keyboard;
+    var currentMeri = keyboard_str[1][2].text;
+
+    if (currentMeri == "AM") {
+      currentMeridium = "PM";
+    } else {
+      currentMeridium = "AM";
+    }
+    keyboard = Keyboard.make(
+      [
+        Key.callback(Buttons[0], "incHours"),
+        Key.callback(Buttons[0], "incMinutes"),
+        Key.callback(Buttons[0], "AMPM"),
+        Key.callback(currentHours, "Noaction"),
+        Key.callback(currentMinutes, "Noaction"),
+        Key.callback(currentMeridium, "Noaction"),
+        Key.callback(Buttons[1], "DecHours"),
+        Key.callback(Buttons[1], "DecMinut"),
+        Key.callback(Buttons[1], "AMPM"),
+        Key.callback("Okay", "Proceed"),
+      ],
+      {
+        columns: 3,
+      }
+    ).inline();
+    ctx.editMessageReplyMarkup(keyboard.reply_markup);
+  });
+
+  bot.action("Proceed", (ctx) => {
+    var time = currentHours + ":" + currentMinutes + " " + currentMeridium;
+    console.log(ctx.callbackQuery.message);
+    ctx.editMessageText(time);
+    ctx.reply(
+      `Donezo..! \n Task scheduled on: ${ScheduleDate} \n Scheduled at: ${time}..!`
     );
   });
 
